f3c80457cce42d85ffdffae10bbe99df0690fdbc
harshsinghs1058 <harshsinghs1058@gmail.com>
2022-06-29 18:03:53 +0530

    input feature added, code optimized

--- execute_code/executePy.js
+++ execute_code/executePy.js
@@ -1,11 +1,10 @@
 const { exec } = require("child_process");
-
-
-
-const executePy = (filepath) => {
-    return new Promise((resolve, reject) => {
+const path = require("path");
+const inputPath = path.join(__dirname, "../inputCompiler");
+const executePy = (filePath, inputFilePath) => {
+  return new Promise((resolve, reject) => {
     exec(
-      `python ${filepath} `,//double back slash to prevent escaping of backticks or dollar sign
+      `python ${filePath} < ${inputFilePath}`, //double back slash to prevent escaping of backticks or dollar sign
       (error, stdout, stderr) => {
         error && reject({ error, stderr });
         stderr && reject(stderr);
