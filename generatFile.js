0f523aa9af16574ba55bac608db80c76f989f45e
harshsinghs1058 <harshsinghs1058@gmail.com>
2022-06-29 17:24:42 +0530

    Input File path corrected

--- generatFile.js
+++ generatFile.js
@@ -1,27 +1,24 @@
-const path = require('path');
-const fs = require('fs');
-const { v4: uuid } = require('uuid');
+const path = require("path");
+const fs = require("fs");
+const { v4: uuid } = require("uuid");
 const dirCodes = path.join(__dirname, "codes");
 const dirCodesInput = path.join(__dirname, "inputCompiler");
-if (!fs.existsSync(dirCodes))
-{
-    fs.mkdirSync(dirCodes, { recursive: true });
+if (!fs.existsSync(dirCodes)) {
+  fs.mkdirSync(dirCodes, { recursive: true });
 }
-if (!fs.existsSync(dirCodesInput))
-{
-    fs.mkdirSync(dirCodesInput, { recursive: true });
+if (!fs.existsSync(dirCodesInput)) {
+  fs.mkdirSync(dirCodesInput, { recursive: true });
 }
-const generateFile = async (format, content,input) => {
-    const jobid = uuid();
-    const filename = `${jobid}.${format}`;
-    const inputfilename=`${jobid}.txt`;
-    const filepath = path.join(dirCodes, filename);
-    const inputfilepath = path.join(dirCodesInput, inputfilename);
-    fs.writeFileSync(filepath, content);
-    fs.writeFileSync(inputfilepath, input);
-    return { filepath, inputfilename };
-    
+const generateFile = async (format, content, input) => {
+  const jobid = uuid();
+  const filename = `${jobid}.${format}`;
+  const inputfilename = `${jobid}.txt`;
+  const filepath = path.join(dirCodes, filename);
+  const inputfilepath = path.join(dirCodesInput, inputfilename);
+  fs.writeFileSync(filepath, content);
+  fs.writeFileSync(inputfilepath, input);
+  return { filepath, inputfilepath };
 };
 module.exports = {
-    generateFile,
+  generateFile,
 };
